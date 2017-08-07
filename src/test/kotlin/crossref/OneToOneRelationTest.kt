ce9fce228f40757f16cee56f4145336159140fd3
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2017-08-07 20:12:44 +0200

    Rewrite tests with Spek

--- src/test/kotlin/crossref/OneToOneRelationTest.kt
+++ src/test/kotlin/crossref/OneToOneRelationTest.kt
@@ -3,67 +3,111 @@ package crossref
 import crossref.impl.SimpleOneToOneRelation
 import crossref.impl.left
 import crossref.impl.right
-import io.kotlintest.matchers.shouldBe
-import io.kotlintest.specs.StringSpec
+import org.amshove.kluent.shouldBe
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
 
+// Declare a relation
+private object A2B : SimpleOneToOneRelation<A, B>()
 
-class OneToOneRelationTest : StringSpec() {
-
-    // Declare a relation
-    object A2B : SimpleOneToOneRelation<A, B>()
-
-    // Declare class A which has a relation to B
-    class A {
-        var b: B? by A2B.right()
-    }
-
-    // Declare class B which has a relation to A
-    class B {
-        var a: A? by A2B.left()
-    }
-
-    init {
-
-        "The references should be null by default" {
-            val a = A()
-            val b = B()
-
-            a.b shouldBe null
-            b.a shouldBe null
-        }
-
-        "If a.b is set, b.a should be set as well" {
-            val a = A()
-            val b = B()
-
-            a.b = b
-
-            b.a shouldBe a
-            a.b shouldBe b
-        }
-
-        "If a.b is unset, b.a should be unset as well" {
-            val a = A()
-            val b = B()
-
-            a.b = b
-            b.a = null
-
-            b.a shouldBe null
-            a.b shouldBe null
-        }
-
-        "If a.b is changed, b.a should be unset in the old reference and set in the new one" {
-            val a = A()
-            val b1 = B()
-            val b2 = B()
-
-            a.b = b1
-            a.b = b2
-
-            a.b shouldBe b2
-            b1.a shouldBe null
-            b2.a shouldBe a
-        }
-    }
+// Declare class A which has a relation to B
+private class A {
+    var b: B? by A2B.right()
 }
+
+// Declare class B which has a relation to A
+private class B {
+    var a: A? by A2B.left()
+}
+
+class OneToOneRelationTest : Spek({
+
+    given("A and B two classes with a OneToOne relation") {
+        // See declaration above
+
+        given("a and b instances of respectively A and B") {
+            var a = A()
+            var b = B()
+
+            beforeEachTest {
+                a = A()
+                b = B()
+            }
+
+            it("should be initialized with no reference") {
+                a.b shouldBe null
+                b.a shouldBe null
+            }
+
+            on("set a referencing b") {
+                a.b = b
+
+                it("should effectively makes a referencing b") {
+                    a.b shouldBe b
+                }
+
+                it("should makes b referencing a as well") {
+                    b.a shouldBe a
+                }
+            }
+
+            on("set b referencing a") {
+                b.a = a
+
+                it("should effectively makes b referencing a") {
+                    b.a shouldBe a
+                }
+
+                it("should makes a referencing b as well") {
+                    a.b = b
+                }
+            }
+
+            given("a and b cross referenced") {
+                a.b = b
+
+                on("set a referencing null") {
+                    a.b = null
+
+                    it("should effectively unset the referance of a") {
+                        a.b shouldBe null
+                    }
+
+                    it("should makes b referencing null as well") {
+                        b.a shouldBe null
+                    }
+                }
+
+                on("set b referencing null") {
+                    b.a = null
+
+                    it("should effectively unset the referance of b") {
+                        b.a shouldBe null
+                    }
+
+                    it("should makes a referencing null as well") {
+                        a.b shouldBe null
+                    }
+                }
+
+                on("set a referencing an other instance") {
+                    a.b = B()
+
+                    it("should makes b referencing null") {
+                        b.a shouldBe null
+                    }
+                }
+
+                on("set b referencing an other instance") {
+                    b.a = A()
+
+                    it("should makes b referencing null") {
+                        a.b shouldBe null
+                    }
+                }
+            }
+        }
+    }
+})
